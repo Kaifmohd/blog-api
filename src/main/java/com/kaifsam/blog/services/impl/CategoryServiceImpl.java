b60af62548191abe7bf2aeaf2c84289ce061c3c4
kaifmohd75 <kaifmohd75@gmail.com>
2023-08-09 23:13:55 +0530

    Created Category API

--- src/main/java/com/kaifsam/blog/services/impl/CategoryServiceImpl.java
+++ src/main/java/com/kaifsam/blog/services/impl/CategoryServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.kaifsam.blog.entities.Category;
 import com.kaifsam.blog.exceptions.ResourceNotFoundException;
@@ -12,6 +13,7 @@ import com.kaifsam.blog.payloads.CategoryDto;
 import com.kaifsam.blog.repositories.CategoryRepo;
 import com.kaifsam.blog.services.CategoryService;
 
+@Service
 public class CategoryServiceImpl implements CategoryService {
 
 	@Autowired
